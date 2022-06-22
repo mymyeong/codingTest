d1474c9239b700b2238e207205f58e7864936c15
taylor <taylor@funnc.com>
2022-06-22 23:40:40 +0900

    코딩테스트 연습용 프로젝트

---
+++ src/main/kotlin/com/mymyeong/codingtest/level1/KthNumber.kt
@@ -0,0 +1,20 @@
+package com.mymyeong.codingtest.level1
+
+class KthNumber {
+
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        val answer = IntArray(commands.size)
+
+        for (index in commands.indices) {
+            val i = commands[index][0]
+            val j = commands[index][1]
+            val k = commands[index][2]
+
+            val temp: IntArray = array.copyOfRange(i - 1, j)
+
+            answer[index] = temp.sorted()[k-1]
+        }
+
+        return answer
+    }
+}
---
+++ src/test/kotlin/com/mymyeong/codingtest/level1/KthNumberTest.kt
@@ -0,0 +1,15 @@
+package com.mymyeong.codingtest.level1
+
+import org.junit.jupiter.api.Test
+
+internal class KthNumberTest {
+
+    private var kthNumber: KthNumber = KthNumber()
+
+    @Test
+    fun test1() {
+        val array : IntArray = intArrayOf(1, 5, 2, 6, 3, 7, 4)
+        val command = arrayOf(intArrayOf(2, 5, 3), intArrayOf(4, 4, 1), intArrayOf(1, 7, 3))
+        assert(kthNumber.solution(array, command).contentEquals(intArrayOf(5, 6, 3)))
+    }
+}
