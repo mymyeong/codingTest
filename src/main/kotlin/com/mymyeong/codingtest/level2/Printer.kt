d81fddabc2f0bbf04eecb34e67c688074af7a614
mymyeong <mymyeong@gmail.com>
2022-06-29 17:13:25 +0900

    프린터 코딩 테스트 코드 코트린 버전으로 업데이트

--- src/main/kotlin/com/mymyeong/codingtest/level2/Printer.kt
+++ src/main/kotlin/com/mymyeong/codingtest/level2/Printer.kt
@@ -4,43 +4,30 @@ import java.util.*
 
 
 class Printer {
-    class PeriodDoc internal constructor(location: Int, priod: Int) {
-        var location = -1
-        var period = 0
-
-        init {
-            this.location = location
-            period = priod
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return location == (other as PeriodDoc?)!!.location
-        }
-
-        override fun hashCode(): Int {
-            var result = location
-            result = 31 * result + period
-            return result
-        }
-    }
+    data class PeriodDoc(
+        var location: Int,
+        var period: Int
+    )
 
     fun solution(priorities: IntArray, location: Int): Int {
         val printQueue = LinkedList<PeriodDoc>()
         val result = LinkedList<PeriodDoc>()
+
         for (i in priorities.indices) {
             printQueue.add(PeriodDoc(i, priorities[i]))
         }
+
         while (!printQueue.isEmpty()) {
             val temp = printQueue.pop()
             if (printQueue.size == 1) {
                 result.addLast(temp)
-            } else if (printQueue.subList(0, printQueue.size).stream().filter { v: PeriodDoc -> v.period > temp.period }
-                    .count() <= 0) {
+            } else if (printQueue.subList(0, printQueue.size).none { v: PeriodDoc -> v.period > temp.period }) {
                 result.add(temp)
             } else {
                 printQueue.add(temp)
             }
         }
+
         return result.indexOf(PeriodDoc(location, priorities[location])) + 1
     }
 }
